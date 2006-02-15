28485f5d22ea9ae40137a9dea95b589a39f00e1b
Harris Michael <harrismic>
2006-02-15 17:43:33 +0000

    Updated to do more extensive output for gx annotation file parsing.
    SVN-Revision: 3626

--- src/gov/nih/nci/ispy/test/GeneExprFileBasedAnnotationServerTester.java
+++ src/gov/nih/nci/ispy/test/GeneExprFileBasedAnnotationServerTester.java
@@ -2,10 +2,14 @@ package gov.nih.nci.ispy.test;
 
 import gov.nih.nci.ispy.service.annotation.GeneExprFileBasedAnnotationService;
 import gov.nih.nci.caintegrator.application.service.annotation.GeneExprAnnotationService;
+import gov.nih.nci.caintegrator.application.service.annotation.ReporterResultset;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 
 public class GeneExprFileBasedAnnotationServerTester {
@@ -37,13 +41,58 @@ public class GeneExprFileBasedAnnotationServerTester {
 		
 		
 		try {
-		  Map gxannotMap = gxannot.getAnnotationsMapForReporters(reporterIds);
+		  Map<String, ReporterResultset> gxannotMap = gxannot.getAnnotationsMapForReporters(reporterIds);
+		  dumpMap(gxannotMap);
 		  System.out.println("mapSize=" + gxannotMap.size());
 		}
 		catch (Exception ex) {
 		  ex.printStackTrace(System.out);
 		}
 
+	}
+	
+	public static void dumpMap(Map<String,ReporterResultset> geneAnnotMap) {
+	
+		Set<String> keys = geneAnnotMap.keySet();
+		ReporterResultset reporterAnnotation;
+		for (String key:keys) {
+		  reporterAnnotation = geneAnnotMap.get(key);
+		  dumpReporterAnnotation(reporterAnnotation);
+		}
+	}
+
+	private static void dumpStringCollection(Collection<String> strings) {
+	
+	  for (Iterator i=strings.iterator(); i.hasNext(); ) {
+		 String str = (String) i.next();
+		 System.out.print(str);
+		 if (i.hasNext()) System.out.print("|");
+	  }
+		
+	}
+	
+	private static void dumpReporterAnnotation(ReporterResultset reporterAnnotation) {
+	  System.out.println("ReporterName=" +reporterAnnotation.getReporter().getValue());
+	  System.out.print("  GeneSymbols:");
+	  dumpStringCollection(reporterAnnotation.getAssociatedGeneSymbols());
+	  System.out.println();
+	  
+	  System.out.print("  GenbankAcc:");
+	  dumpStringCollection(reporterAnnotation.getAssiciatedGenBankAccessionNos());
+	  System.out.println();
+	  
+	  System.out.print("  LocusLink: ");
+	  dumpStringCollection(reporterAnnotation.getAssociatedLocusLinkIDs());
+	  System.out.println();
+	  
+	  System.out.print("  Pathways");
+	  dumpStringCollection(reporterAnnotation.getAssociatedPathways());
+	  System.out.println();
+	  
+	  System.out.print("  GO:");
+	  dumpStringCollection(reporterAnnotation.getAssociatedGOIds());
+	  System.out.println();
+	  System.out.println("=============================");
 	}
 
 }
