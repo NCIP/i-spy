8f5bfe50030128e93147e15d54b3bb39a8bb20e4
Harris Michael <harrismic>
2006-02-22 21:20:20 +0000

    Removed methods.
    SVN-Revision: 3666

--- src/gov/nih/nci/ispy/dto/query/ISPYClassComparisonQueryDTO.java
+++ src/gov/nih/nci/ispy/dto/query/ISPYClassComparisonQueryDTO.java
@@ -1,19 +1,18 @@
 package gov.nih.nci.ispy.dto.query;
 
 import gov.nih.nci.caintegrator.dto.query.ClassComparisonQueryDTO;
-import gov.nih.nci.ispy.service.clinical.ClinicalDataSpecifier;
 import gov.nih.nci.ispy.service.clinical.TimepointType;
 
 import java.util.List;
 
 public interface ISPYClassComparisonQueryDTO extends ClassComparisonQueryDTO{
 	
-	public ClinicalDataSpecifier getBaselineGrpSpecifier(); 
-	
-	public void setBaselineGrpSpecifier(ClinicalDataSpecifier baselineGrpSpecifier);
-	
-	public ClinicalDataSpecifier getComparisonGrpSpecifier(); 
-	
-	public void setComparisonGrpSpecifier(ClinicalDataSpecifier comparisonGrpSpecifier);
-	
+//	public ISPYclinicalDataQueryDTO getBaselineGrpSpecifier(); 
+//	
+//	public void setBaselineGrpSpecifier(ISPYclinicalDataQueryDTO baselineGrpSpecifier);
+//	
+//	public ISPYclinicalDataQueryDTO getComparisonGrpSpecifier(); 
+//	
+//	public void setComparisonGrpSpecifier(ISPYclinicalDataQueryDTO comparisonGrpSpecifier);
+//	
 }
