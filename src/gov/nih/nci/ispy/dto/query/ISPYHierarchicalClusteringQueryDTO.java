4e36f715c3686351da9589d48a6e063852c964a2
Harris Michael <harrismic>
2006-03-01 21:25:38 +0000

    Fixed format.
    SVN-Revision: 3724

--- src/gov/nih/nci/ispy/dto/query/ISPYHierarchicalClusteringQueryDTO.java
+++ src/gov/nih/nci/ispy/dto/query/ISPYHierarchicalClusteringQueryDTO.java
@@ -2,8 +2,9 @@ package gov.nih.nci.ispy.dto.query;
 
 import java.util.List;
 
-import gov.nih.nci.caintegrator.dto.query.HierarchicalClusteringQueryDTO;import gov.nih.nci.ispy.service.clinical.TimepointType;
-;
+import gov.nih.nci.caintegrator.dto.query.HierarchicalClusteringQueryDTO;
+import gov.nih.nci.ispy.service.clinical.TimepointType;
+
 
 public interface ISPYHierarchicalClusteringQueryDTO extends HierarchicalClusteringQueryDTO {
 	public void setTimepoints(List<TimepointType> timepoints);
