3c07095ac6440e08fc06d541f08a58ef6fbf5e20
Harris Michael <harrismic>
2006-04-11 19:20:38 +0000

    More functionality
    SVN-Revision: 3811

--- src/gov/nih/nci/ispy/test/IdMapperFileBasedServiceTester.java
+++ src/gov/nih/nci/ispy/test/IdMapperFileBasedServiceTester.java
@@ -3,7 +3,7 @@ package gov.nih.nci.ispy.test;
 import java.util.ArrayList;
 import java.util.List;
 
-import gov.nih.nci.ispy.service.annotation.IdMapperEntry;
+import gov.nih.nci.ispy.service.annotation.RegistrantInfo;
 import gov.nih.nci.ispy.service.annotation.IdMapperFileBasedService;
 
 public class IdMapperFileBasedServiceTester {
@@ -19,31 +19,31 @@ public class IdMapperFileBasedServiceTester {
 	public static void main(String[] args) {
 		
 		IdMapperFileBasedService idMapper = IdMapperFileBasedService.getInstance();
-		idMapper.setMappingFile("C:\\eclipse\\workspace\\ispyportal\\WebRoot\\WEB-INF\\data_files\\idMappingFile4-6-06.txt");
+		idMapper.setMappingFile("C:\\eclipse\\workspace\\ispyportal\\WebRoot\\WEB-INF\\data_files\\ID_Mapping_4-11-06.txt");
 		 
 		List<String> inputList = new ArrayList<String>();
 		inputList.add("212283");
 		inputList.add("1024");
-		List<IdMapperEntry> entries = idMapper.getMapperEntriesForIds(inputList);
+		List<RegistrantInfo> entries = idMapper.getMapperEntriesForIds(inputList);
 
-		for (IdMapperEntry entry:entries) {
+		for (RegistrantInfo entry:entries) {
 		  System.out.println(entry);
 		}
 		
-		String[][] idArr = idMapper.getMappingForIds(inputList);
-		
-		for (int row=0; row < idArr.length; row++) {
-		  String[] rowData = idArr[row];
-		  for (int col=0; col < rowData.length; col++) {
-		    System.out.print(rowData[col]);
-		    if (col < rowData.length-1) {
-		    	System.out.print("\t");
-		    }
-		    else {
-		    	System.out.println();
-		    }
-		  }
-		}
+//		String[][] idArr = idMapper.getMappingForIds(inputList);
+//		
+//		for (int row=0; row < idArr.length; row++) {
+//		  String[] rowData = idArr[row];
+//		  for (int col=0; col < rowData.length; col++) {
+//		    System.out.print(rowData[col]);
+//		    if (col < rowData.length-1) {
+//		    	System.out.print("\t");
+//		    }
+//		    else {
+//		    	System.out.println();
+//		    }
+//		  }
+//		}
 		
 	}
 
