2191335a02ed91d186bfe31b48237b3109525316
no_author <no_author@no_author.cvs2svn>
2006-06-13 16:09:38 +0000

    This commit was manufactured by cvs2svn to create tag 'ISPY-ANALYSIS-PORTAL_RC3_12JUNE06'.
    SVN-Revision: 4015

---
+++ ISPY-ANALYSIS-PORTAL_RC3_12JUNE06/WebRoot/WEB-INF/jsp/tiles/group_tile.jsp
@@ -0,0 +1,140 @@
+<%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %>
+<%@ taglib uri="/WEB-INF/struts-logic.tld" prefix="logic" %>
+<%@ taglib uri="/WEB-INF/ispy.tld" prefix="app" %>
+<%@ page import="java.util.*, gov.nih.nci.ispy.web.struts.form.*" %> 
+
+
+<fieldset class="gray">
+<legend class="red">Select Group<b class="req">*</b>
+<app:help help="If you have chosen to compare groups with ONE fixed timepoint, only move two groups into the selected groups box. Your baseline group will be determined by the second group in the box. If you have chosen to compare groups ACROSS timepoints, you can move one group into the selected groups box. Your baseline group is now determined by the first timepoint you have chosen." />
+</legend>
+
+<logic:present name="principalComponentForm">
+
+<html:radio property="groupsOption" styleClass="radio" value="allSamples" onclick="onRadio(this,'allSamples');" />Show all samples<br /><br />
+
+<html:radio property="groupsOption" styleId="variousSamplesRadio" styleClass="radio" value="variousSamples" onclick="onRadio(this,'variousSamples');" />Select samples<br />
+<table align="center" border="0">
+    <tr style="vertical-align:top">
+    	<td>Existing Groups
+			<br/>
+			<html:select styleId="nonselectedGroups" size="5" multiple="true" style="width:200px" property="existingGroups">
+			     <html:optionsCollection property="existingGroupsList"/>
+			</html:select>
+		</td>
+		<td style="vertical-align:middle">
+			<input id="button1" onclick="preMove(document.getElementById('variousSamplesRadio').checked, document.getElementById('selectedGroups'),document.getElementById('nonselectedGroups'))" value="<<" type="button" disabled="true" /><br />
+			<input id="button2" onclick="preMove(document.getElementById('variousSamplesRadio').checked, document.getElementById('nonselectedGroups'),document.getElementById('selectedGroups'))" value=">>" type="button" disabled="true" />
+		</td>
+		<td>Selected Groups
+			<br/>
+			<html:select styleId="selectedGroups" size="5" multiple="true" style="width:200px" property="selectedGroups">
+			</html:select>
+		</td>
+	</tr>
+</table>
+</logic:present>
+
+
+<logic:present name="classComparisonForm">
+	<span id="instruction"><b>&nbsp;</b></span>
+	<html:errors property="selectedGroups1"/>
+	<html:errors property="selectedGroups2"/>
+	<html:errors property="selectedGroups3"/>
+	<table align="center" border="0">
+    	<tr style="vertical-align:top">
+		<td>Existing Groups
+	        <br/>
+	        <html:select styleId="nonselectedGroups" size="5" multiple="true" style="width:200px" property="existingGroups">
+	           <html:optionsCollection property="existingGroupsList"/>
+			</html:select>
+		</td>
+		<td style="vertical-align:middle">
+			<input onclick="move($('selectedGroups'),$('nonselectedGroups'));initBaseline();" value="&lt;&lt;" type="button"/><br />
+			<input onclick="move($('nonselectedGroups'),$('selectedGroups'));initBaseline();" value=">>" type="button"/>
+		</td>
+		<td>Selected Groups
+			<br/>
+			<html:select styleId="selectedGroups" size="5" multiple="true" style="width:200px; overflow:none;" property="selectedGroups">
+			</html:select>
+			<br/>	
+			
+			<div style="padding-top:10px; padding-left:5px;">
+				<span style="cursor:pointer; border:1px solid; padding-right:3px; padding-left:3px;" onclick="javascript:moveUpList(document.getElementById('selectedGroups'));initBaseline();">&uarr;</span>
+				<span style="cursor:pointer; border:1px solid; padding-right:3px; padding-left:3px;" onclick="javascript:moveDownList(document.getElementById('selectedGroups'));initBaseline();">&darr;</span>
+				<span style="font-size:10px; font-family:arial; padding:10px;">
+					Baseline : <span id="baseline">none</span>
+					<input type="hidden" name="baselineGroup" id="baselineGroup"/>
+				</span>
+			</div>
+		</td>
+		</tr>
+	</table>
+  
+  <script language="javascript">
+	var lst = document.getElementById('selectedGroups');
+	var nonlst = document.getElementById('nonselectedGroups');
+		
+	var bltag = " (baseline)";
+
+	function initBaseline()	{
+		
+		//assumes two choices only for now
+		var _timepoint = "";
+		if(document.getElementsByName('timepointRange') && document.getElementsByName('timepointRange').length > 0)	{
+			_timepoint = document.getElementsByName('timepointRange')[0].checked ? "fixed" : "across";
+		}
+		
+		//changes instruction text
+		var instruction = (_timepoint == "across") ? "<em><b>choose one group</b></em>" : "<em><b>choose 2 groups</b></em>";
+		document.getElementById("instruction").innerHTML = instruction;
+					
+		var baseline = lst[lst.length-1];
+		
+		var lengthToDo = (_timepoint == "across") ? lst.length : lst.length-1;
+		
+		//remove the tag from the other ones
+		for(var i=0; i<lengthToDo; i++)	{
+			var currentBaseline = lst[i];
+			if(currentBaseline.text.indexOf(bltag) != -1)	{
+				currentBaseline.text = currentBaseline.text.substring(0, currentBaseline.text.indexOf(bltag));
+				currentBaseline.style.color = '';
+				currentBaseline.style.border = '';
+			}
+		}
+				
+		for(var i=0; i<nonlst.length; i++)	{
+			var currentBaseline = nonlst[i];
+			if(currentBaseline.text.indexOf(bltag) != -1)	{
+				currentBaseline.text = currentBaseline.text.substring(0, currentBaseline.text.indexOf(bltag));
+				currentBaseline.style.color = '';
+				currentBaseline.style.border = '';
+			}
+		}		
+		
+		//dont add baseline for across
+		if(_timepoint == "across")	{
+			$("baseline").innerHTML = $F('timepointBaseline');
+			$F("baselineGroup") = $F('timepointBaseline');
+			return;
+		}
+		
+		//add the tag to the new baseline
+		if(baseline != null && baseline.text.indexOf(bltag) == -1)	{
+			baseline.text += bltag;
+			baseline.style.color="red";
+			baseline.style.border="1px solid";
+			var cleanbaseline = baseline.text.substring(0, baseline.text.indexOf(bltag));
+			
+			document.getElementById("baseline").innerHTML = cleanbaseline.length > 10 ? cleanbaseline.substring(0, 10)+"..." : cleanbaseline ;
+			document.getElementById("baselineGroup").value = baseline.value;
+		}
+		
+	
+	}
+	
+	//initBaseline();
+</script>
+</logic:present>
+
+</fieldset>
---
+++ ISPY-ANALYSIS-PORTAL_RC3_12JUNE06/src/gov/nih/nci/ispy/web/ajax/DynamicListHelper.java
@@ -0,0 +1,194 @@
+package gov.nih.nci.ispy.web.ajax;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
+import gov.nih.nci.caintegrator.application.lists.ListManager;
+import gov.nih.nci.caintegrator.application.lists.ListType;
+import gov.nih.nci.caintegrator.application.lists.UserList;
+import gov.nih.nci.caintegrator.application.lists.UserListBean;
+import gov.nih.nci.caintegrator.application.lists.UserListBeanHelper;
+import gov.nih.nci.ispy.util.ispyConstants;
+import gov.nih.nci.ispy.web.helper.ISPYListValidator;
+
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang.StringUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+
+import uk.ltd.getahead.dwr.ExecutionContext;
+
+public class DynamicListHelper {
+
+	
+	public DynamicListHelper() {}
+	
+	
+	public static String getListAsList(ListType ty){
+		String results = "";
+		
+		HttpSession session = ExecutionContext.get().getSession(false);
+		UserListBeanHelper helper = new UserListBeanHelper(session);
+                      
+        List patientLists = helper.getLists(ty);
+        if (!patientLists.isEmpty()) {
+            for (int i = 0; i < patientLists.size(); i++) {
+                UserList list = (UserList) patientLists.get(i);
+                ListManager uploadManager = (ListManager) ListManager.getInstance();
+                Map paramMap = uploadManager.getParams(list);
+                String commas = StringUtils.join(list.getList().toArray(), ",");
+                results += ("<li id='" + paramMap.get("listName") + "' title='"+commas+"'>"+paramMap.get("listName")+"</li>");
+            }
+        } else {
+            results = "";
+        }
+
+		return results;
+	}
+	
+    public static String getDefaultPatientListAsList() {
+        return DynamicListHelper.getListAsList(ListType.DefaultPatientDID);
+    }
+    public static String getPatientListAsList()	{
+		return DynamicListHelper.getListAsList(ListType.PatientDID);
+	}
+	
+	public static String getGeneListAsList()	{
+		return DynamicListHelper.getListAsList(ListType.GeneSymbol);
+	}
+	
+	public static String createGenericList(ListType type, String[] list, String name){
+		
+		//no duplicates
+		HashSet<String> h = new HashSet<String>();
+		for (int i = 0; i < list.length; i++)
+			h.add(list[i].trim());
+		List<String> cleanList = new ArrayList<String>();
+		for(String n : h)	{
+			cleanList.add(n);
+		}
+		
+		String success = "fail";
+		ListManager um = ListManager.getInstance();
+        ISPYListValidator listValidator = new ISPYListValidator();
+		try	{
+			UserList mylist = um.createList(type, name, cleanList, listValidator);
+			UserListBeanHelper ulbh = new UserListBeanHelper();
+			ulbh.addList(mylist);
+			success = "pass";
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+		return success;
+	}
+	
+	public static String createPatientList(String[] list, String name){
+		//create list w/ type=patient
+		return DynamicListHelper.createGenericList(ListType.PatientDID, list, name);
+	}
+    
+    public static String createDefaultPatientList(String[] list, String name){
+        //create list w/ type=defaultPatient
+        return DynamicListHelper.createGenericList(ListType.DefaultPatientDID, list, name);
+    }
+	
+	public static String createGeneList(String[] list, String name){
+		return DynamicListHelper.createGenericList(ListType.GeneSymbol, list, name);
+	}
+
+	public static String exportListasTxt(String name, HttpSession session){
+		String txt = "";
+		
+		UserListBeanHelper helper = new UserListBeanHelper(session);
+		
+        List<String> listItems = helper.getItemsFromList(name);
+        txt = StringUtils.join(listItems.toArray(), "\r\n");
+		return txt;
+	}
+	/*
+	//accept commas seperated lists too
+	public static String createGeneList(String commaList, String name){
+		String[] list = StringUtils.split(commaList, ",");
+		return DynamicListHelper.createGenericList(ListType.GeneSymbol, list, name);
+	}
+	
+	public static String createPatientList(String commaList, String name){
+		String[] list = StringUtils.split(commaList, ",");
+		return DynamicListHelper.createGenericList(ListType.PatientDID, list, name);
+	}
+	*/
+	
+	public static Document getAllLists(String listType){
+		
+	   //HttpSession session = ExecutionContext.get().getSession(false);
+	   //ISPYUserListBeanHelper helper = new ISPYUserListBeanHelper(session);
+	   UserListBeanHelper helper = new UserListBeanHelper();
+	   
+	   Collection<String> myLists = new ArrayList<String>();
+	   
+	   Document listDoc = DocumentHelper.createDocument();
+	   Element list = listDoc.addElement("lists");
+	   
+	   if(listType.equals(ListType.PatientDID.toString()))	{
+		   list.addAttribute("type", "patient");
+		   myLists = helper.getPatientListNames();
+	   }
+       else if(listType.equals(ListType.DefaultPatientDID.toString())) {
+           myLists = helper.getDefaultPatientListNames();     
+           list.addAttribute("type", "defaultPatient");
+       }
+	   else	if(listType.equals(ListType.GeneSymbol.toString()))	{
+		   myLists = helper.getGeneSymbolListNames();     
+		   list.addAttribute("type", "gene");
+	   }
+	   
+	   for(String listName : myLists){
+		 UserList ul = helper.getUserList(listName);
+		 DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm aaa", Locale.US);
+		 if(ul!=null)	
+			 list.addElement("list").addAttribute("name", ul.getName()).addAttribute("date", dateFormat.format(ul.getDateCreated()).toString()).addAttribute("items", String.valueOf(ul.getItemCount())).addAttribute("invalid", String.valueOf(ul.getInvalidList().size()));
+	   }
+	   
+	   
+	   return listDoc;
+	}
+	
+	public static String uniteLists(String[] sLists, String groupName, String groupType, String action)	{
+		String results = "pass";
+		
+		UserListBeanHelper helper = new UserListBeanHelper();
+		try	{
+			List<String> al = Arrays.asList(sLists);
+			ListType lt = groupType.equals("gene") ? ListType.GeneSymbol : ListType.PatientDID;
+			if(action.equals("join"))	{
+				helper.uniteLists(al, groupName, lt);
+			}
+			else	{
+                if(helper.isIntersection(al)){
+                    helper.intersectLists(al, groupName, lt);
+                }
+                else results = "fail";
+			}
+		}
+		catch(Exception e){
+			results = "fail";
+		}
+		
+		results += "," + groupType + "," + action;
+		return results;
+	}
+
+}
