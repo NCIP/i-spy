445580d6aafc81b94da360a1ac2a5ce35e6ae29a
Ryan Landy <landyr>
2006-07-05 19:09:19 +0000

    initial
    SVN-Revision: 4044

---
+++ src/gov/nih/nci/ispy/util/ISPYListFilter.java
@@ -0,0 +1,19 @@
+package gov.nih.nci.ispy.util;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.Set;
+
+import gov.nih.nci.caintegrator.application.lists.ListType;
+
+public class ISPYListFilter {
+	
+	public static ListType[] values()	{
+
+		ListType[] lsa = {ListType.GeneSymbol}; //no duplicates here
+		
+		return lsa;
+	}
+}
