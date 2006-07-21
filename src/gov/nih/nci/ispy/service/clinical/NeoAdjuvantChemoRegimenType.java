59e2931b4c15f45a7e144c4b3491cfd6ec6796b6
Harris Michael <harrismic>
2006-07-21 16:27:33 +0000

    New type to allow query on NeoAdjuvantChemRegimen
    SVN-Revision: 4058

---
+++ src/gov/nih/nci/ispy/service/clinical/NeoAdjuvantChemoRegimenType.java
@@ -0,0 +1,26 @@
+package gov.nih.nci.ispy.service.clinical;
+
+public enum NeoAdjuvantChemoRegimenType {
+	AC,
+	EC,
+	FEC,
+	FAC,
+	A,
+	AC_Td,
+	AC_Tp,
+	A_Td,
+	A_Tp,
+	AC_TdH,
+	AC_TpH,
+	FEC_Tp,
+	EC_Tp,
+	AC_TpTd,
+	A_Tp_C,
+	ACV_Tp,
+	AC_Td_Xeloda,
+	EC_Tp_Carboplatin,
+	FEC_Tp_Abraxane,
+	AC_Td_Navelbine_Xeloda,
+	AC_Tp_Vinorelbine_Tarcena
+   
+}
