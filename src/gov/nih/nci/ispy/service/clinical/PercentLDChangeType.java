c7d6945ce5102ee10c6b9bb60528e2abd15f3615
Harris Michael <harrismic>
2006-07-21 19:07:24 +0000

    New Enum type for % ld change.
    SVN-Revision: 4060

---
+++ src/gov/nih/nci/ispy/service/clinical/PercentLDChangeType.java
@@ -0,0 +1,9 @@
+package gov.nih.nci.ispy.service.clinical;
+
+import java.io.Serializable;
+
+public enum PercentLDChangeType implements Serializable {
+  PERCENT_LD_CHANGE_T1_T2,
+  PERCENT_LD_CHANGE_T1_T3,
+  PERCENT_LD_CHANGE_T1_T4
+}
