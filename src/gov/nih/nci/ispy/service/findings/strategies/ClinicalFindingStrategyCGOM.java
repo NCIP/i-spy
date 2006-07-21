2f8bb2cbb87ddccaa2aeec81a7e5726d8065c53b
Harris Michael <harrismic>
2006-07-21 19:27:54 +0000

    Changes to support ClinicalFindingStrategy abstraction.
    SVN-Revision: 4062

--- src/gov/nih/nci/ispy/service/findings/strategies/ClinicalFindingStrategyCGOM.java
+++ src/gov/nih/nci/ispy/service/findings/strategies/ClinicalFindingStrategyCGOM.java
@@ -1,7 +1,6 @@
 package gov.nih.nci.ispy.service.findings.strategies;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 import gov.nih.nci.caintegrator.dto.query.QueryDTO;
@@ -10,45 +9,40 @@ import gov.nih.nci.caintegrator.exceptions.FindingsQueryException;
 import gov.nih.nci.caintegrator.exceptions.ValidationException;
 import gov.nih.nci.caintegrator.service.findings.Finding;
 import gov.nih.nci.ispy.dto.query.ISPYclinicalDataQueryDTO;
-import gov.nih.nci.ispy.service.findings.ISPYClinicalFinding;
-
+import gov.nih.nci.ispy.service.clinical.ClinicalCGOMBasedQueryService;
 import gov.nih.nci.ispy.service.clinical.ClinicalFileBasedQueryService;
 import gov.nih.nci.ispy.service.clinical.PatientData;
+import gov.nih.nci.ispy.service.findings.ISPYClinicalFinding;
 
 /**
- * Strategy to get clinical findings from a file.
- * @author harrismic
+ * Strategy to get Clinical findings from the CGOM.
  *
  */
-
-public class ClinicalFindingStrategyFile extends ClinicalFindingStrategy {
+public class ClinicalFindingStrategyCGOM extends ClinicalFindingStrategy {
 
 	private ISPYClinicalFinding clinicalFinding;
 	
-	public ClinicalFindingStrategyFile(String sessionId, String taskId, ISPYclinicalDataQueryDTO queryDTO) throws ValidationException {
+	public ClinicalFindingStrategyCGOM(String sessionId, String taskId, ISPYclinicalDataQueryDTO queryDTO) throws ValidationException {
 		super(sessionId, taskId, queryDTO);
 	}
 
 	public boolean validate(QueryDTO query) throws ValidationException {
-	  return true;
+		// TODO Auto-generated method stub
+		return true;
 	}
 
 	public boolean createQuery() throws FindingsQueryException {
+		// TODO Auto-generated method stub
 		return true;
 	}
 
 	public boolean executeQuery() throws FindingsQueryException {
-		//take the query dto and execute a query based on the criteria
-		ClinicalFileBasedQueryService cqs = ClinicalFileBasedQueryService.getInstance();
+		ClinicalCGOMBasedQueryService cqs = ClinicalCGOMBasedQueryService.getInstance();
 		
 		
 		Set<PatientData> patientData = cqs.getClinicalData(getQueryDTO());
 		
-	    //Set<String> patientDIDs = cqs.getPatientDIDs(getQueryDTO());
-		
-	    //List<PatientData> patientData = cqs.getPatientDataForPatientDIDs(new ArrayList<String>(patientDIDs));
-	    
-	    
+	   
 	    
 	    //put the result into the finding 
 	    clinicalFinding = new ISPYClinicalFinding(this.getSessionId(), this.getTaskId(), this.getQueryDTO());
