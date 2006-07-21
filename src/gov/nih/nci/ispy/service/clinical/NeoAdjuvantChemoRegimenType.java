811f3e4ae0ddcc4f0889ac741c0bba9da81b9d98
Harris Michael <harrismic>
2006-07-21 17:16:52 +0000

    Added toString and getLookupString methods.
    SVN-Revision: 4059

--- src/gov/nih/nci/ispy/service/clinical/NeoAdjuvantChemoRegimenType.java
+++ src/gov/nih/nci/ispy/service/clinical/NeoAdjuvantChemoRegimenType.java
@@ -1,26 +1,48 @@
 package gov.nih.nci.ispy.service.clinical;
 
 public enum NeoAdjuvantChemoRegimenType {
-	AC,
-	EC,
-	FEC,
-	FAC,
-	A,
-	AC_Td,
-	AC_Tp,
-	A_Td,
-	A_Tp,
-	AC_TdH,
-	AC_TpH,
-	FEC_Tp,
-	EC_Tp,
-	AC_TpTd,
-	A_Tp_C,
-	ACV_Tp,
-	AC_Td_Xeloda,
-	EC_Tp_Carboplatin,
-	FEC_Tp_Abraxane,
-	AC_Td_Navelbine_Xeloda,
-	AC_Tp_Vinorelbine_Tarcena
+	AC   { public String toString() { return "AC"; }
+		   public String getLookupString() { return toString();}} ,
+	EC   { public String toString() { return "EC"; }
+	       public String getLookupString() { return toString();}} ,
+	FEC  { public String toString() { return "FAC"; }
+	       public String getLookupString() { return toString();}},
+	FAC  { public String toString() { return "FAC"; }
+		   public String getLookupString() { return toString();}},
+	A    { public String toString() { return "A"; }
+	       public String getLookupString() { return toString();}},
+	AC_TD { public String toString() { return "AC->Td"; }
+	        public String getLookupString() { return toString();}},
+	AC_TP { public String toString() { return "AC->Tp"; }
+			public String getLookupString() { return toString();}},
+	A_TD { public String toString() { return "A->Td"; }
+		   public String getLookupString() { return toString();}},
+	A_TP { public String toString() { return "A->Tp"; }
+		   public String getLookupString() { return toString();}},
+	AC_TDH { public String toString() { return "AC->TdH"; }
+			 public String getLookupString() { return toString();}},
+	AC_TPH { public String toString() { return "AC->TpH"; }
+			 public String getLookupString() { return toString();}},
+	FEC_TP { public String toString() { return "FEC->Tp"; }
+			 public String getLookupString() { return toString();}},
+	EC_TP { public String toString() { return "EC->Tp"; }
+	   		public String getLookupString() { return toString();}},
+	AC_TPTD { public String toString() { return "AC->TpTd"; }
+			  public String getLookupString() { return toString();}},
+	A_TP_C { public String toString() { return "A->Tp->C"; }
+			 public String getLookupString() { return toString();}},
+	ACV_TP { public String toString() { return "ACV->Tp"; }
+	   		 public String getLookupString() { return toString();}},
+	AC_TD_XELODA { public String toString() { return "AC->Td->Xeloda"; }
+	  			   public String getLookupString() { return toString();}},
+	EC_TP_CARBOPLATIN { public String toString() { return "EC->Tp->Carboplatin"; }
+						public String getLookupString() { return toString();}},
+	FEC_TP_ABRAXANE { public String toString() { return "FEC->Tp->Abraxane"; }
+					  public String getLookupString() { return toString();}},
+	AC_TD_NAVELBINE_XELODA { public String toString() { return "AC->Td->Navelbine->Xeloda"; }
+	 						 public String getLookupString() { return toString();}},
+	AC_TP_VINORELBINE_TARCENA { public String toString() { return "AC->Tp->Vinorelbine->Tarcena"; }
+	 							public String getLookupString() { return toString();}};
    
+	public abstract String getLookupString();
 }
