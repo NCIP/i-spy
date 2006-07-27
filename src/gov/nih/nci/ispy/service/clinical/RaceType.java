4ddbaba3ccd537925d7b02b61a9add142bfd0141
Harris Michael <harrismic>
2006-07-27 16:44:02 +0000

    RaceType
    SVN-Revision: 4077

---
+++ src/gov/nih/nci/ispy/service/clinical/RaceType.java
@@ -0,0 +1,9 @@
+package gov.nih.nci.ispy.service.clinical;
+
+public enum RaceType {
+	American_Indian_or_Alaska_Native,
+	Asian,
+	Black_or_African_American,
+	Native_Hawaiian_or_Pacific_Islander,
+	White
+}
