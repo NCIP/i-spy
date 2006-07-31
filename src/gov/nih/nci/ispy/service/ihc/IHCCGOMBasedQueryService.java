a0a22a8cd35337bd55e98c76b732bbf572562742
Harris Michael <harrismic>
2006-07-31 18:31:57 +0000

    IHC CGOM query service
    SVN-Revision: 4095

---
+++ src/gov/nih/nci/ispy/service/ihc/IHCCGOMBasedQueryService.java
@@ -0,0 +1,24 @@
+package gov.nih.nci.ispy.service.ihc;
+
+import gov.nih.nci.ispy.dto.query.IHCqueryDTO;
+
+import java.util.Set;
+
+public class IHCCGOMBasedQueryService implements IHCDataService {
+
+	public IHCCGOMBasedQueryService() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Set<String> getPatientDIDs(IHCqueryDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set<IHCData> getIHCData(IHCqueryDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
