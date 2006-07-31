8c9edf075c2359bd28534254a6792c2fd032b1e4
Harris Michael <harrismic>
2006-07-31 18:05:37 +0000

    Starting to implement IHC
    SVN-Revision: 4094

---
+++ src/gov/nih/nci/ispy/service/ihc/IHCDataService.java
@@ -0,0 +1,14 @@
+package gov.nih.nci.ispy.service.ihc;
+
+
+import gov.nih.nci.ispy.dto.query.IHCqueryDTO;
+
+import java.util.Set;
+
+public interface IHCDataService {
+
+	public Set<String> getPatientDIDs(IHCqueryDTO dto);
+	
+	public Set<IHCData> getIHCData(IHCqueryDTO dto);
+	
+}
---
+++ src/gov/nih/nci/ispy/service/ihc/IHCFileBasedQueryService.java
@@ -0,0 +1,25 @@
+package gov.nih.nci.ispy.service.ihc;
+
+import gov.nih.nci.ispy.dto.query.IHCqueryDTO;
+
+import java.util.Set;
+
+public class IHCFileBasedQueryService implements IHCDataService {
+
+	public IHCFileBasedQueryService() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Set<String> getPatientDIDs(IHCqueryDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set<IHCData> getIHCData(IHCqueryDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
