59ccbed4dc15552af235daed61ae178c39900961
Harris Michael <harrismic>
2006-08-15 17:36:18 +0000

    Getting Correlation working.
    SVN-Revision: 4109

---
+++ src/gov/nih/nci/ispy/dto/query/CorrelationQueryDTO.java
@@ -0,0 +1,22 @@
+package gov.nih.nci.ispy.dto.query;
+
+import gov.nih.nci.caintegrator.dto.query.QueryDTO;
+
+public class CorrelationQueryDTO implements QueryDTO {
+
+	public CorrelationQueryDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setQueryName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getQueryName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
