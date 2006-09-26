d3eb8475fbd6577e9c5ca92a01fbed727ea722d8
Kevin Rosso <Kevin.Rosso@fda.hhs.gov>
2006-09-26 20:16:40 +0000

    Legal page and login fixes
    SVN-Revision: 4155

--- src/gov/nih/nci/ispy/web/struts/action/LoginAction.java
+++ src/gov/nih/nci/ispy/web/struts/action/LoginAction.java
@@ -33,84 +33,84 @@ public final class LoginAction extends Action
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response) throws IOException
 	{
-		LoginForm f = (LoginForm) form;
-		String userName=f.getUserName();
-		String password = f.getPassword();
-		
-		boolean valid = false;		
-		HttpSession session = request.getSession();
-        FileInputStream fsi = null;
-
-		Properties props = new Properties();
-		ServletContext context = session.getServletContext();
-        
-	    try {				   
-        
-			fsi = new FileInputStream(context.getRealPath("WEB-INF")+ SEPERATOR + "users.properties"); 
-			logger.debug("loading props...");
-			props.load(fsi);
-			logger.debug("props file length: " + props.size());	
-			fsi.close();
-			String authType = props.getProperty("authentication") ;
-			
-			if(authType != null && authType.equals("file"))
-			{
-				String u = "";
-				String p = "";			
-			
-				u = props.getProperty("username");
-				p = props.getProperty("password");
-									
-				if(userName.equals(u) && password.equals(p))
-				{
-					valid = true ;
-				}
-			}
-			else if(authType != null && authType.equals("ldap"))
-			{
-			    UserCredentials user = null;	    			
-				SecurityManager secManager =  SecurityManager.getInstance("ispy") ;
-				user = secManager.authenticate(userName,password);
-				if(user != null)
-				{
-					valid = true ;
-				}						
-			}
-				    	
-						
-			
-			if(valid)
-			{
-				session.setAttribute("logged", "yes");
-                session.setAttribute("name", userName);
-                                   
-                UserListBean userListBean = new UserListBean();
-
-                ISPYListValidator listValidator = new ISPYListValidator();
-                
-                String filePath = System.getProperty("gov.nih.nci.ispyportal.data_directory");
-                
-                userListBean = ISPYListLoader.loadLists(userListBean,ispyConstants.ALL_USER_LISTS, filePath, listValidator);  
-                // load IspySpecific clinical status lists
-                userListBean = ISPYListLoader.loadStatusGroups(userListBean);
-                //add userListBean to session
-                session.setAttribute(CacheConstants.USER_LISTS,userListBean);
-                
-				return (mapping.findForward("success"));	
-			}
-			
-		} catch (Exception e) {
-		    logger.error("Can't read user props");
-		    logger.error(e);
-            if(fsi != null)
-                fsi.close();
-          }
-          finally   {
-            if(fsi != null)
-                fsi.close();
-         }
-		
-		if(valid)
+        LoginForm f = (LoginForm) form;
+//		String userName=f.getUserName();
+//		String password = f.getPassword();
+//		
+//		boolean valid = false;		
+//		HttpSession session = request.getSession();
+//        FileInputStream fsi = null;
+//
+//		Properties props = new Properties();
+//		ServletContext context = session.getServletContext();
+//        
+//	    try {				   
+//        
+//			fsi = new FileInputStream(context.getRealPath("WEB-INF")+ SEPERATOR + "users.properties"); 
+//			logger.debug("loading props...");
+//			props.load(fsi);
+//			logger.debug("props file length: " + props.size());	
+//			fsi.close();
+//			String authType = props.getProperty("authentication") ;
+//			
+//			if(authType != null && authType.equals("file"))
+//			{
+//				String u = "";
+//				String p = "";			
+//			
+//				u = props.getProperty("username");
+//				p = props.getProperty("password");
+//									
+//				if(userName.equals(u) && password.equals(p))
+//				{
+//					valid = true ;
+//				}
+//			}
+//			else if(authType != null && authType.equals("ldap"))
+//			{
+//			    UserCredentials user = null;	    			
+//				SecurityManager secManager =  SecurityManager.getInstance("ispy") ;
+//				user = secManager.authenticate(userName,password);
+//				if(user != null)
+//				{
+//					valid = true ;
+//				}						
+//			}
+//				    	
+//						
+//			
+//			if(valid)
+//			{
+//				session.setAttribute("logged", "yes");
+//                session.setAttribute("name", userName);
+//                                   
+//                UserListBean userListBean = new UserListBean();
+//
+//                ISPYListValidator listValidator = new ISPYListValidator();
+//                
+//                String filePath = System.getProperty("gov.nih.nci.ispyportal.data_directory");
+//                
+//                userListBean = ISPYListLoader.loadLists(userListBean,ispyConstants.ALL_USER_LISTS, filePath, listValidator);  
+//                // load IspySpecific clinical status lists
+//                userListBean = ISPYListLoader.loadStatusGroups(userListBean);
+//                //add userListBean to session
+//                session.setAttribute(CacheConstants.USER_LISTS,userListBean);
+//                
+//				return (mapping.findForward("success"));	
+//			}
+//			
+//		} catch (Exception e) {
+//		    logger.error("Can't read user props");
+//		    logger.error(e);
+//            if(fsi != null)
+//                fsi.close();
+//          }
+//          finally   {
+//            if(fsi != null)
+//                fsi.close();
+//         }
+//		
+		if(f.isUserLoggedIn())
 			return (mapping.findForward("success"));
 		else
 			return (mapping.findForward("failure"));
