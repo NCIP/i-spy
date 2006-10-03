66ad58725b6c4afd73d4d95450c9e908c9e7c773
Dana Zhang <zhangd>
2006-10-03 19:46:50 +0000

    changed it from a class to an interface
    SVN-Revision: 4165

--- src/gov/nih/nci/ispy/service/ihc/IHCData.java
+++ src/gov/nih/nci/ispy/service/ihc/IHCData.java
@@ -1,18 +1,5 @@
 package gov.nih.nci.ispy.service.ihc;
 
-public class IHCData {
-
-	public IHCData() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+public interface IHCData {
 
 }
