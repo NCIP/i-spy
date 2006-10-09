bd0688e2d5be56ff004ab81cfc81ef7ee40c2e66
Harris Michael <harrismic>
2006-10-09 21:15:13 +0000

    Added static method.
    SVN-Revision: 4187

--- src/gov/nih/nci/ispy/service/clinical/ContinuousType.java
+++ src/gov/nih/nci/ispy/service/clinical/ContinuousType.java
@@ -68,9 +68,16 @@ public enum ContinuousType implements Serializable {
 	PERCENT_LD_CHANGE { public String toString() { return "PERCENT_LD_CHANGE";}},
     PERCENT_LD_CHANGE_T1_T2{ public String toString() { return "PERCENT LD CHANGE_T1_T2";}},
     PERCENT_LD_CHANGE_T1_T3{ public String toString() { return "PERCENT_LD_CHANGE_T1_T3";}},
-    PERCENT_LD_CHANGE_T1_T4{ public String toString() { return "PERCENT_LD_CHANGE_T1_T4";}}
+    PERCENT_LD_CHANGE_T1_T4{ public String toString() { return "PERCENT_LD_CHANGE_T1_T4";}};
 	
-	
+	public static TimepointType getEndpoint(ContinuousType ct) {
+	  switch(ct) {
+	  case PERCENT_LD_CHANGE_T1_T2 : return TimepointType.T2;
+	  case PERCENT_LD_CHANGE_T1_T3: return TimepointType.T3;
+	  case PERCENT_LD_CHANGE_T1_T4: return TimepointType.T4;
+	  }
+	  return null;
+	}
 	
 }
 
