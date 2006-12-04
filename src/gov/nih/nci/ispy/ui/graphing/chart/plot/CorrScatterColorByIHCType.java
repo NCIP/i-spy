410082a7196d2f3e06d1bff9e6c0705096d56790
Harris Michael <harrismic>
2006-12-04 19:39:15 +0000

    Changed to reflect new way of drawing plots.
    SVN-Revision: 4277

--- src/gov/nih/nci/ispy/ui/graphing/chart/plot/CorrScatterColorByIHCType.java
+++ src/gov/nih/nci/ispy/ui/graphing/chart/plot/CorrScatterColorByIHCType.java
@@ -3,10 +3,10 @@ package gov.nih.nci.ispy.ui.graphing.chart.plot;
 import java.awt.Color;
 
 public enum CorrScatterColorByIHCType {
-	X_POS_Y_POS {public Color getColor() {return Color.red; }},
-	X_POS_Y_NEG {public Color getColor() {return Color.orange; }},
-	X_NEG_Y_POS {public Color getColor() {return Color.yellow; }},
-	X_NEG_Y_NEG {public Color getColor() {return Color.green; }};
+	POSITIVE {public Color getColor() {return Color.GREEN; }},
+	INTERMEDIATE {public Color getColor() {return Color.yellow; }},
+	NEGATIVE {public Color getColor() {return Color.red; }},
+	NA {public Color getColor() {return Color.GRAY; }};
 	
 	public abstract Color getColor();
 }
