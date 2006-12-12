25c1601114d5ad7b168def39e74c72c7a8a2e771
no_author <no_author@no_author.cvs2svn>
2006-12-12 05:49:19 +0000

    This commit was manufactured by cvs2svn to create tag 'ispy_1_0_0_0_deploy-stage_build_10'.
    SVN-Revision: 4324

---
+++ ispy_1_0_0_0_deploy-stage_build_10/WebRoot/helpDocs/ISPY_Online_Help1.0/wwhdata/js/search/pairs/pair13.js
@@ -0,0 +1,23 @@
+function FileData_Pairs(x)
+{
+x.t("describes","i-spy");
+x.t("sample","identifiers.");
+x.t("sample","identifier");
+x.t("overview","looking");
+x.t("looking","patient");
+x.t("conducting","identifier");
+x.t("i-spy","lookup");
+x.t("identifiers","sample");
+x.t("include:","lookup");
+x.t("identifiers.","topics");
+x.t("section","describes");
+x.t("section","include:");
+x.t("identifier","lookup");
+x.t("patient","sample");
+x.t("patient","identifiers");
+x.t("topics","section");
+x.t("lookup","overview");
+x.t("lookup","conducting");
+x.t("lookup","section");
+x.t("lookup","patient");
+}
