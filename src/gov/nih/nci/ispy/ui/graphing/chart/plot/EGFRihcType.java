27df01c34540f39d3898f858aa1a97028054b33a
Harris Michael <harrismic>
2007-02-26 16:37:03 +0000

    Changed color scheme to have RED be positive and GREEN negative.
    SVN-Revision: 4384

--- src/gov/nih/nci/ispy/ui/graphing/chart/plot/EGFRihcType.java
+++ src/gov/nih/nci/ispy/ui/graphing/chart/plot/EGFRihcType.java
@@ -3,8 +3,8 @@ package gov.nih.nci.ispy.ui.graphing.chart.plot;
 import java.awt.Color;
 
 public enum EGFRihcType {
-   NEGATIVE {public Color getColor() {return Color.RED; }}, 
-   POSITIVE {public Color getColor() {return Color.GREEN; }};
+   NEGATIVE {public Color getColor() {return Color.GREEN; }}, 
+   POSITIVE {public Color getColor() {return Color.RED; }};
    
    public static String getOfficialGeneSymbol() { return "EGFR"; }
    
