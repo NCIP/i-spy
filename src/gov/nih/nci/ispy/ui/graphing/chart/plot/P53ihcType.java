fa7dc73ba63326d7b82df27cf0a551e9c70c9de5
Harris Michael <harrismic>
2007-02-26 16:30:16 +0000

    Switched expression colors to have RED overexpressed and GREEN NO_Overexpressed.
    SVN-Revision: 4381

--- src/gov/nih/nci/ispy/ui/graphing/chart/plot/P53ihcType.java
+++ src/gov/nih/nci/ispy/ui/graphing/chart/plot/P53ihcType.java
@@ -3,8 +3,8 @@ package gov.nih.nci.ispy.ui.graphing.chart.plot;
 import java.awt.Color;
 
 public enum P53ihcType {
-	OVEREXPRESSED {public Color getColor() {return Color.GREEN; }},
-	NO_OVEREXPRESSION {public Color getColor() {return Color.RED; }}; 
+	OVEREXPRESSED {public Color getColor() {return Color.RED; }},
+	NO_OVEREXPRESSION {public Color getColor() {return Color.GREEN; }}; 
 	
 	public static String getOfficialGeneSymbol() { return "TP53"; }
 	
