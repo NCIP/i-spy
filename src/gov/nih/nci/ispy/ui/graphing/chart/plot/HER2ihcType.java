55d74bef7e81566d741eeb6967b1939d0bc87e92
Harris Michael <harrismic>
2007-02-26 16:36:06 +0000

    Changed color scheme to have RED be overexpressed and GREEN underexpressed.
    SVN-Revision: 4383

--- src/gov/nih/nci/ispy/ui/graphing/chart/plot/HER2ihcType.java
+++ src/gov/nih/nci/ispy/ui/graphing/chart/plot/HER2ihcType.java
@@ -3,10 +3,10 @@ package gov.nih.nci.ispy.ui.graphing.chart.plot;
 import java.awt.Color;
 
 public enum HER2ihcType {
-	BORDERLINE {public Color getColor() {return Color.YELLOW; }},
-	NO_OVEREXPRESSION {public Color getColor() {return Color.RED; }},
-	OVEREXPRESSED {public Color getColor() {return Color.GREEN; }},
-	WEAK_OVEREXPRESSED {public Color getColor() {return Color.ORANGE; }};
+	BORDERLINE {public Color getColor() {return Color.ORANGE; }},
+	NO_OVEREXPRESSION {public Color getColor() {return Color.GREEN; }},
+	OVEREXPRESSED {public Color getColor() {return Color.RED; }},
+	WEAK_OVEREXPRESSED {public Color getColor() {return Color.YELLOW; }};
 	
 	public static String getOfficialGeneSymbol() { return "ERBB2"; }
 	
