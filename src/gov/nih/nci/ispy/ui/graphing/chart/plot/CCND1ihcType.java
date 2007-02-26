56d24e9cb823f1bea1ba97b54259a687aa39a747
Harris Michael <harrismic>
2007-02-26 16:38:21 +0000

    Changed color scheme to that RED be high expression and GREEN low expression.
    SVN-Revision: 4385

--- src/gov/nih/nci/ispy/ui/graphing/chart/plot/CCND1ihcType.java
+++ src/gov/nih/nci/ispy/ui/graphing/chart/plot/CCND1ihcType.java
@@ -3,8 +3,8 @@ package gov.nih.nci.ispy.ui.graphing.chart.plot;
 import java.awt.Color;
 
 public enum CCND1ihcType {
-	HIGH_EXPRESSION {public Color getColor() {return Color.GREEN; }},
-	LOW_EXPRESSION {public Color getColor() {return Color.RED; }}; 
+	HIGH_EXPRESSION {public Color getColor() {return Color.RED; }},
+	LOW_EXPRESSION {public Color getColor() {return Color.GREEN; }}; 
 	
 	public static String getOfficialGeneSymbol() { return "CCND1"; }
 	
