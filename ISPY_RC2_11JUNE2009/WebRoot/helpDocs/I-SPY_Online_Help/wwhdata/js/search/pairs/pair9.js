14417f4c452b8bd1af711fc38872ed6018eada5e
Shuang Cai <cais>
2009-06-11 20:16:44 +0000

    This tag is for 2009 April clinic data update.
    SVN-Revision: 4846

---
+++ ISPY_RC2_11JUNE2009/WebRoot/helpDocs/I-SPY_Online_Help/wwhdata/js/search/pairs/pair9.js
@@ -0,0 +1,23 @@
+function FileData_Pairs(x)
+{
+x.t("getting","started");
+x.t("access","i-spy");
+x.t("menu","i-spy");
+x.t("i-spy","menu");
+x.t("i-spy","i-spy");
+x.t("i-spy","functions");
+x.t("i-spy","side");
+x.t("i-spy","workspace");
+x.t("i-spy","tabs");
+x.t("enables","quick");
+x.t("started","i-spy");
+x.t("quick","access");
+x.t("functions","information");
+x.t("functions","tabs");
+x.t("side","bar");
+x.t("workspace","getting");
+x.t("workspace","i-spy");
+x.t("workspace","enables");
+x.t("information","access");
+x.t("tabs","i-spy");
+}
